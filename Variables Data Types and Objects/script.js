615d46a6f33fb6a9f9bc3e8ffaf393d466e1cb3a
abdultalha0862 <20211a0405@bvrit.ac.in>
2025-04-29 10:08:35 +0530

    Added initial JavaScript file with variable declarations and console logs

---
+++ Variables Data Types and Objects/script.js
@@ -0,0 +1,18 @@
+console.log("Welcome to this World");
+
+let a=90;
+a+=1;
+let b="String";
+console.log(a,b);
+
+console.log(typeof a, typeof b);
+
+
+let a1=90;
+let a2="king";
+console.log(a1+a2); 
+// The output of the above line is "90king" 
+
+let x1="Car below";
+let x2=22;
+let x3=3.55;
