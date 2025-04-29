a778fc602242c4701ba870cf5e42af17807388af
abdultalha0862 <20211a0405@bvrit.ac.in>
2025-04-29 10:40:31 +0530

    Added JavaScript file for Conditional Statements with grading logic

---
+++ Conditional Statements/script.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+
+// let age=45;
+
+// if(age>=18){
+//     console.log("You can Vote");
+// }
+// else{
+//     console.log("You can vote");
+// }
+
+
+let marks=45;
+
+if(marks>=90){
+    console.log("Your Grade is A")
+}
+
+else if(marks>=90){
+    console.log("Your Grade is B")
+}
+
+else if(marks>=90){
+    console.log("Your Grade is C")
+}
+
+else{
+    console.log("Your Grade is D");    
+}
