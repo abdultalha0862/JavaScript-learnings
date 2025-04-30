c64db4429a8ff6c9215fdb894cc6edafd1352dd1
abdultalha0862 <20211a0405@bvrit.ac.in>
2025-04-30 22:41:03 +0530

    Addedl HTML and JavaScript files for Looping Statements

---
+++ Looping Statements/script.js
@@ -0,0 +1,30 @@
+// console.log("INtroduction to Loops");
+
+
+// For Loop
+
+// for(let i=0;i<=10;i++){
+//     console.log(i);    
+// }
+
+// for(let i=10;i>=1;i--){
+//     console.log(i);
+    
+// }
+
+
+// let obj={
+//     "name":["Sam","Atlman"],
+//     "age":[20,21]
+// }
+
+// for(let i in obj){
+//     console.log(i);
+// }
+
+let i=0;
+
+while(i<=10){
+    console.log(i);
+    i++;
+}
