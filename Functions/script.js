faa16f1e7751bed6f473e21d096ae6999be045b3
abdultalha0862 <20211a0405@bvrit.ac.in>
2025-05-01 10:21:00 +0530

    Added  JavaScript functions and arrow function example

---
+++ Functions/script.js
@@ -0,0 +1,31 @@
+// function greet(name){
+//     console.log("Hey "+name+ " Good Morning");
+// }
+
+// greet("Sam");
+
+
+// function add(a,b){
+//     console.log(a+b);    
+// }
+
+// add(10,20);
+
+
+// function add(a,b,c=80){
+//     return a+b+c;
+// }
+
+// let res=add(10,20);
+
+// console.log(res);
+
+
+let fun1=(para)=>{
+    return " i am arrow "+para;
+    
+}
+
+fres=fun1(15);
+
+console.log(fres);
