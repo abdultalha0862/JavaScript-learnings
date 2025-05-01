cf0da69b61ebed13e53b8ca818add0c9abce5391
abdultalha0862 <20211a0405@bvrit.ac.in>
2025-05-01 19:53:32 +0530

    Added JavaScript string manipulation examples

---
+++ Strings/script.js
@@ -0,0 +1,47 @@
+// console.log("This is the String Code");
+
+// let str1="Abdul Talha";
+
+// console.log(str1[0]);
+// console.log(str1[1]);
+// console.log(str1[2]);
+// console.log(str1[3]);
+// console.log(str1[4]);
+// console.log(str1[5]);
+
+
+// for(let i=0;i<=str1.length;i++){
+//     console.log(str1[i]);
+// }
+
+// console.log(str1.length);
+
+
+// let st1_1="Abdul";
+// let str_2="Talha";
+
+// console.log("hey "+st1_1+ " "+str_2 );
+// console.log(`hey ${st1_1} ${str_2} is Awesome`);
+
+
+let str="Abdul Talha";
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.charAt(0));
+
+console.log(str.replace("A","B"));
+
+console.log(str.slice(1,4));
+
+console.log(str.slice(2));
+
+
+
+
+
+
+
+
+
+
