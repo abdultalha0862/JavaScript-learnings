0e45341112db131586227264e5a14567a4cf1311
abdultalha0862 <20211a0405@bvrit.ac.in>
2025-05-05 20:19:32 +0530

    added script.js with array examples

---
+++ Arrays/script.js
@@ -0,0 +1,42 @@
+let arr=[12,241,3512,426,565];
+// console.log(arr);
+
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// console.log(typeof arr[2]);
+
+// console.log(typeof arr);
+
+// console.log(arr.join(" "));
+
+// console.log(arr.length);
+
+// console.log(arr.toString());
+
+
+// console.log(arr.join(" "));
+
+
+// arr.push(45);
+// console.log(arr);
+
+// let arr1=[1,2,4546,343];
+// let arr2=["abc","ght","bjfhb"];
+
+// console.log(arr1.concat(arr2));
+
+
+// cons
+
+
+
+
+
+
+
+
+
+
