c3344dd7ab5df507b41db0f1dc0f83275d08f902
abdultalha0862 <20211a0405@bvrit.ac.in>
2025-05-07 14:11:55 +0530

    Added script.js for DOM manipulation examples

---
+++ DOM- Selecting by Ids/script.js
@@ -0,0 +1,6 @@
+let boxes=document.getElementsByClassName("box");
+console.log(boxes);
+
+document.getElementsByClassName("box")[4].style.background="green";
+document.getElementsByClassName("box")[0].style.color="red";
+document.getElementById("black").style.background="aqua";
